d1f9de14f5936571ed357c46da5eb20c9e85b07d
zoro-diablo <vyshnavk117@gmail.com>
2024-12-11 17:11:43 +0530

    updated the hero section

--- src/components/SmoothScrollHero.jsx
+++ src/components/SmoothScrollHero.jsx
@@ -1,17 +1,18 @@
-import { ReactLenis } from "lenis/dist/lenis-react";
+import { ReactLenis } from 'lenis/dist/lenis-react';
 import {
   motion,
   useMotionTemplate,
   useScroll,
   useTransform,
-} from "framer-motion";
-import { SiSpacex } from "react-icons/si";
-import { FiArrowRight, FiMapPin } from "react-icons/fi";
-import { useRef } from "react";
+} from 'framer-motion';
+import { FiArrowRight, FiMapPin } from 'react-icons/fi';
+import { useRef } from 'react';
+import genfoxheroimage from '../assets/GenFox.png';
+import logo from '../assets/logogen.png';
 
 export const SmoothScrollHero = () => {
   return (
-    <div className="bg-white">
+    <div className='bg-white'>
       <ReactLenis
         root
         options={{
@@ -31,17 +32,17 @@ export const SmoothScrollHero = () => {
 
 const Nav = () => {
   return (
-    <nav className="fixed left-0 right-0 top-0 z-50 flex items-center justify-between px-6 py-3 text-white">
-      <SiSpacex className="text-3xl mix-blend-difference bg-black" />
+    <nav className='fixed left-0 right-0 top-0 z-50 flex items-center justify-between px-6 py-3 text-white'>
+      <img src={logo} className='h-10' alt="logo" />
       <button
         onClick={() => {
-          document.getElementById("launch-schedule")?.scrollIntoView({
-            behavior: "smooth",
+          document.getElementById('launch-schedule')?.scrollIntoView({
+            behavior: 'smooth',
           });
         }}
-        className="flex items-center gap-1 text-xs text-zinc-400"
+        className='flex items-center gap-1 text-xs text-zinc-400'
       >
-        LAUNCH SCHEDULE <FiArrowRight />
+        COMING SOON <FiArrowRight />
       </button>
     </nav>
   );
@@ -53,13 +54,13 @@ const Hero = () => {
   return (
     <div
       style={{ height: `calc(${SECTION_HEIGHT}px + 100vh)` }}
-      className="relative w-full"
+      className='relative w-full'
     >
       <CenterImage />
 
       <ParallaxImages />
 
-      <div className="absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-zinc-950" />
+      <div className='absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-zinc-950/0 to-zinc-950' />
     </div>
   );
 };
@@ -75,7 +76,7 @@ const CenterImage = () => {
   const backgroundSize = useTransform(
     scrollY,
     [0, SECTION_HEIGHT + 500],
-    ["170%", "100%"]
+    ['170%', '100%']
   );
   const opacity = useTransform(
     scrollY,
@@ -85,15 +86,14 @@ const CenterImage = () => {
 
   return (
     <motion.div
-      className="sticky top-0 h-screen w-full"
+      className='sticky top-0 h-screen w-full'
       style={{
         clipPath,
         backgroundSize,
         opacity,
-        backgroundImage:
-          "url(https://static01.nyt.com/images/2023/09/20/multimedia/19bizsky-google-bard-print/19GOOGLE-BARD-top-hpgz-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
+        backgroundImage: `url(${genfoxheroimage})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
       }}
     />
   );
@@ -101,34 +101,34 @@ const CenterImage = () => {
 
 const ParallaxImages = () => {
   return (
-    <div className="mx-auto max-w-5xl px-4 pt-[200px]">
+    <div className='mx-auto max-w-5xl px-4 pt-[200px]'>
       <ParallaxImg
-        src="https://play-lh.googleusercontent.com/MWV1erZURmTaeXGj29ZLWMSo_7DB92q3IL71lDSRooqbb3qidsa4c9DJ0_jEQgOeXEQ=w416-h235-rw"
-        alt="And example of a space launch"
+        src='https://play-lh.googleusercontent.com/MWV1erZURmTaeXGj29ZLWMSo_7DB92q3IL71lDSRooqbb3qidsa4c9DJ0_jEQgOeXEQ=w416-h235-rw'
+        alt='And example of a space launch'
         start={-200}
         end={200}
-        className="w-1/3"
+        className='w-1/3'
       />
       <ParallaxImg
-        src="https://cdn.mos.cms.futurecdn.net/Un5CRWVYRDC769ZrkZKAjn-1200-80.jpg"
-        alt="An example of a space launch"
+        src='https://cdn.mos.cms.futurecdn.net/Un5CRWVYRDC769ZrkZKAjn-1200-80.jpg'
+        alt='An example of a space launch'
         start={200}
         end={-250}
-        className="mx-auto w-2/3"
+        className='mx-auto w-2/3'
       />
       <ParallaxImg
-        src="https://cdn.idropnews.com/wp-content/uploads/2021/03/24190921/Google-Fit-Logo.jpg"
-        alt="Orbiting satellite"
+        src='https://cdn.idropnews.com/wp-content/uploads/2021/03/24190921/Google-Fit-Logo.jpg'
+        alt='Orbiting satellite'
         start={-200}
         end={200}
-        className="ml-auto w-1/3"
+        className='ml-auto w-1/3'
       />
       <ParallaxImg
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEY23ecDss1sXWoP8GC6yuww9MGwuoFoBWcQ&s"
-        alt="Orbiting satellite"
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEY23ecDss1sXWoP8GC6yuww9MGwuoFoBWcQ&s'
+        alt='Orbiting satellite'
         start={0}
         end={-500}
-        className="ml-24 w-5/12"
+        className='ml-24 w-5/12'
       />
     </div>
   );
@@ -162,24 +162,24 @@ const ParallaxImg = ({ className, alt, src, start, end }) => {
 const Schedule = () => {
   return (
     <section
-      id="launch-schedule"
-      className="mx-auto max-w-5xl px-4 py-48 text-white"
+      id='launch-schedule'
+      className='mx-auto max-w-5xl px-4 py-48 text-white'
     >
       <motion.h1
         initial={{ y: 48, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
-        transition={{ ease: "easeInOut", duration: 0.75 }}
-        className="mb-20 text-4xl font-black uppercase text-zinc-700"
+        transition={{ ease: 'easeInOut', duration: 0.75 }}
+        className='mb-20 text-4xl font-black uppercase text-zinc-700'
       >
         Coming soon
       </motion.h1>
-      <ScheduleItem title="NG-21" date="Dec 9th" location="Florida" />
-      <ScheduleItem title="Starlink" date="Dec 20th" location="Texas" />
-      <ScheduleItem title="Starlink" date="Jan 13th" location="Florida" />
-      <ScheduleItem title="Turksat 6A" date="Feb 22nd" location="Florida" />
-      <ScheduleItem title="NROL-186" date="Mar 1st" location="California" />
-      <ScheduleItem title="GOES-U" date="Mar 8th" location="California" />
-      <ScheduleItem title="ASTRA 1P" date="Apr 8th" location="Texas" />
+      <ScheduleItem title='NG-21' date='Dec 9th' location='Florida' />
+      <ScheduleItem title='Starlink' date='Dec 20th' location='Texas' />
+      <ScheduleItem title='Starlink' date='Jan 13th' location='Florida' />
+      <ScheduleItem title='Turksat 6A' date='Feb 22nd' location='Florida' />
+      <ScheduleItem title='NROL-186' date='Mar 1st' location='California' />
+      <ScheduleItem title='GOES-U' date='Mar 8th' location='California' />
+      <ScheduleItem title='ASTRA 1P' date='Apr 8th' location='Texas' />
     </section>
   );
 };
@@ -189,14 +189,14 @@ const ScheduleItem = ({ title, date, location }) => {
     <motion.div
       initial={{ y: 48, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
-      transition={{ ease: "easeInOut", duration: 0.75 }}
-      className="mb-9 flex items-center justify-between border-b border-zinc-800 px-3 pb-9"
+      transition={{ ease: 'easeInOut', duration: 0.75 }}
+      className='mb-9 flex items-center justify-between border-b border-zinc-800 px-3 pb-9'
     >
       <div>
-        <p className="mb-1.5 text-xl text-zinc-50">{title}</p>
-        <p className="text-sm uppercase text-zinc-500">{date}</p>
+        <p className='mb-1.5 text-xl text-zinc-50'>{title}</p>
+        <p className='text-sm uppercase text-zinc-500'>{date}</p>
       </div>
-      <div className="flex items-center gap-1.5 text-end text-sm uppercase text-zinc-500">
+      <div className='flex items-center gap-1.5 text-end text-sm uppercase text-zinc-500'>
         <p>{location}</p>
         <FiMapPin />
       </div>
